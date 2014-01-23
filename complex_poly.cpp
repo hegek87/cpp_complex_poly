ca146fdce6f153f8d298d4937ad345ac089f6eab
hegek87 <hegelandka@vcu.edu>
2014-01-23 10:06:50 -0500

    Changed the code of overloaded << for a more natural looking printed Complex polynomial

--- complex_poly.cpp
+++ complex_poly.cpp
@@ -21,16 +21,46 @@ Complex ComplexPoly::evaluate(Complex val){
 }
 
 std::ostream& operator<<(std::ostream& os, const ComplexPoly& cp){
-	os << "(" << cp.coeffs[0] << ")";
+	double mag = cp.coeffs[0].modulus();
+	if(mag == 0 || (mag == 1 && cp.coeffs[0].getReal() == 1)
+			|| (mag == 1 && cp.coeffs[0].getImag() == 1)){
+		os << cp.coeffs[0];
+	}
+	else{
+		os << "(" << cp.coeffs[0] << ")";
+	}
+	
 	if(!cp.degree){ return os; }
 	for(int i = 0; i < cp.degree; ++i){
-		if(i == 0){
-			os << " + (" << cp.coeffs[i+1] << ")";
-			os << "z";
+		mag = cp.coeffs[i+1].modulus();
+		if(!mag){ continue; }
+		if((mag == 1 && cp.coeffs[i+1].getReal() == 1)){
+			os << " + ";
+			if(!i){
+				os << "z";
+			}
+			else{
+				os << "z^" << i+1;
+			}
+		}
+		else if(mag == 1 && cp.coeffs[i+1].getImag() == 1){
+			os << cp.coeffs[i+1];
+			if(!i){
+				os << "z";
+			}
+			else{
+				os << "z^" << i+1;
+			}
 		}
 		else{
-			os << " + (" << cp.coeffs[i+1] << ")";
-			os << "z^" << i+1;
+			if(!i){
+				os << " + (" << cp.coeffs[i+1] << ")";
+				os << "z";
+			}
+			else{
+				os << " + (" << cp.coeffs[i+1] << ")";
+				os << "z^" << i+1;
+			}
 		}
 	}
 	return os;
